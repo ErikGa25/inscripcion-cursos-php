27466934c91c42fac8d1fcf2cbf993010467256c
Erik García <jerik67@gmail.com>
2016-07-17 21:31:29 -0500

    Proyecto Laravel

--- APPINSCRIPCIONCURSOS/app/routes.php
+++ APPINSCRIPCIONCURSOS/app/routes.php
@@ -10,6 +10,7 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+
 Route::get('/', 'AlumnoController@index');
 Route::any('/login', 'AlumnoController@login');
 Route::any('/registro', 'AlumnoController@registrarAlumno');
